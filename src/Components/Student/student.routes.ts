bdf738401c4286903e4d3b4b2ba3f978709f598b
kevit-keval-kankrecha <kevit.keval.kankrecha@gmail.com>
2023-10-20 09:39:21 +0530

    Solved merge conflict

--- src/Components/Student/student.routes.ts
+++ src/Components/Student/student.routes.ts
@@ -1,42 +1,41 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import studentController from './student.controller';
 import authentication from '../../utils/authentication';
 import authorization from '../../utils/authorization';
 
 class studentRoutes {
-    public router: Router;
+  public router: Router;
 
-    studentController = new studentController();
+  studentController = new studentController();
 
-    constructor() {
-        this.router = Router();
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.router = Router();
+    this.initializeRoutes();
+  }
 
-    initializeRoutes() {
-        //Create New Student
-        this.router.post('/add', authentication, authorization, this.studentController.createStudent);
+  initializeRoutes() {
+    //Create New Student
+    this.router.post('/add', authentication, authorization, this.studentController.createStudent);
 
-        //Login Student
-        this.router.post('/login', this.studentController.loginStudent);
+    //Login Student
+    this.router.post('/login', this.studentController.loginStudent);
 
-        //LogOut Students
-        this.router.post('/logout', authentication, authorization, this.studentController.logOutStudent);
+    //LogOut Students
+    this.router.post('/logout', authentication, authorization, this.studentController.logOutStudent);
 
-        //List Student
-        this.router.get('/', authentication, authorization, this.studentController.getStudents);
+    //List Student
+    this.router.get('/', authentication, authorization, this.studentController.getStudents);
 
-        //Update Student
-        this.router.patch('/update/:id?', authentication, authorization, this.studentController.updateStudent);
+    //Update Student
+    this.router.patch('/update/:id?', authentication, authorization, this.studentController.updateStudent);
 
-        //Delete Student
-        this.router.delete('/delete/:id?', authentication, authorization, this.studentController.deleteStudent);
+    //Delete Student
+    this.router.delete('/delete/:id?', authentication, authorization, this.studentController.deleteStudent);
 
-        //Get Profile
-        this.router.get('/me', authentication, authorization, this.studentController.getProfile);
-    }
-
+    //Get Profile
+    this.router.get('/me', authentication, authorization, this.studentController.getProfile);
+  }
 }
 
-export default new studentRoutes().router
+export default new studentRoutes().router;
